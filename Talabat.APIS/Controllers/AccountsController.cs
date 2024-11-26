deb2f9b82d0e30768ab1b9a830cbc85d8f702fea
mohye <mohye20@gmail.com>
2024-11-26 12:36:36 +0200

    Register Endpoint

---
+++ Talabat.APIS/Controllers/AccountsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.APIS.DTOs;
+using Talabat.APIS.Errors;
+using Talabat.Core.Entites.Identity;
+
+namespace Talabat.APIS.Controllers
+{
+	public class AccountsController : ApiBaseController
+	{
+		private readonly UserManager<AppUser> _userManager;
+
+		public AccountsController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		// Register
+
+		[HttpPost("Register")]
+		public async Task<ActionResult<UserDTO>> Register(RegisterDTO model)
+		{
+			var User = new AppUser()
+			{
+				DisplayName = model.DisplayName,
+				Email = model.Email,
+				UserName = model.Email.Split('@')[0],
+				PhoneNumber = model.PhoneNumber
+			};
+
+			var Result = await _userManager.CreateAsync(User, model.Password);
+
+			if (!Result.Succeeded)
+			{
+				return BadRequest(new ApiResponse(400));
+			}
+
+			var ReturnedUser = new UserDTO()
+			{
+				DisplayName = User.DisplayName,
+				Email = User.Email,
+				Token = "ThisWillBeToken"
+			};
+
+			return ReturnedUser; 
+		}
+	}
+}
--- Talabat.APIS/obj/Debug/net6.0/Talabat.APIS.AssemblyInfo.cs
+++ Talabat.APIS/obj/Debug/net6.0/Talabat.APIS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Talabat.APIS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a9842aa66baef7199c13aab5c24d582d2da478")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a25cc25ee7909a5b27c39f0891e14275a399aed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Talabat.APIS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Talabat.APIS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
