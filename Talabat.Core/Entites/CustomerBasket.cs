d0d523a96c393c0a57a10e87ed88af5fdd1ac87b
mohye <mohye20@gmail.com>
2024-11-26 08:46:51 +0200

    Basket Module Entities

---
+++ Talabat.Core/Entites/CustomerBasket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entites
+{
+	internal class CustomerBasket
+	{
+		public decimal Id { get; set; }
+
+		public List<BasketItem> Items { get; set; }
+	}
+}
